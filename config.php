1e17b9063b56058f4cdd909ad61c6e5c2a114795
Abdallah Mouhmed Fall <abdallahmouhamed.fall@unchk.edu.sn>
2026-06-27 13:28:09 +0000

    premier commit

---
+++ config.php
@@ -0,0 +1,21 @@
+<?php
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "educlass";
+
+
+$conn = new mysqli(
+    $host,
+    $user,
+    $password,
+    $database
+);
+
+
+if ($conn->connect_error) {
+    die("Erreur de connexion : " . $conn->connect_error);
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+
+session_start();
+
+
+if (isset($_SESSION['user'])) {
+
+    header("Location: dashboard.php");
+
+} else {
+
+    header("Location: login.php");
+
+}
+
+exit();
+
+?>
