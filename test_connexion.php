2d26bba8ed555f3a584deeddb52987505c795fcc
abdallahmouhamedfall <abdallahmouhamed.fall@unchk.edu.sn>
2026-07-08 22:53:05 +0000

    Delete test_connexion.php

--- test_connexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once __DIR__ . "/../config/config.php";
-
-if ($conn) {
-    echo "<h2 style='color:green'>Connexion à la base de données réussie ✅</h2>";
-} else {
-    echo "<h2 style='color:red'>Erreur de connexion ❌</h2>";
-}
